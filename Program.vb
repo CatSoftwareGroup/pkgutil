5956e155bebe7f0840315bf8823507896ea55b35
Hawk2811 <2811.pedro@gmail.com>
2026-02-18 20:40:42 -0300

    Add Help Dialog

--- Program.vb
+++ Program.vb
@@ -1,7 +1,32 @@
 ﻿Module Program
 
+    Dim args() As String = Environment.GetCommandLineArgs()
+
     Sub Main()
+        If args.Length < 3 Then
+            If args(1) = "--help" Then
+                Call HelpDialog()
+            End If
+        Else
+            Call UsageDialog()
+        End If
+    End Sub
+
+
+    Public Sub InstallPKG()
 
     End Sub
 
+    Public Sub UsageDialog()
+        Console.WriteLine("Invalid Args! Try pkgutil --help")
+    End Sub
+
+    Public Sub HelpDialog()
+        Console.WriteLine("Lukindu Package utility")
+        Console.WriteLine()
+        Console.WriteLine("--add - install new package")
+        Console.WriteLine("--info - get info of package")
+        Console.WriteLine("--del - uninstall package")
+        Console.WriteLine("--version - version of PKGUtil")
+    End Sub
 End Module
