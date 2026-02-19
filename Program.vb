8e561ff728e5ca1252176d9f6bd64a69d8214a6b
Hawk2811 <2811.pedro@gmail.com>
2026-02-19 18:51:45 -0300

    Add Install function

--- Program.vb
+++ Program.vb
@@ -12,7 +12,19 @@ Module Program
             If args(1) = "--help" Then
                 Call HelpDialog()
             ElseIf args(1) = "--add" Then
-                Call InstallPKG()
+                If args.Length < 3 Then
+                    Console.WriteLine("No Package Selected!")
+                Else
+                    If My.Computer.FileSystem.FileExists(args(2)) Then
+                        If Path.GetExtension(args(2)) = ".pkg" Then
+                            Call InstallDialog()
+                        Else
+                            Call InvalidPKG()
+                        End If
+                    Else
+                        Console.WriteLine("No Package Selected!")
+                    End If
+                End If
             ElseIf args(1) = "--info" Then
                 Call PKGInfo()
             ElseIf args(1) = "--del" Then
@@ -26,12 +38,59 @@ Module Program
     End Sub
 
 
+    Public Sub InstallDialog()
+        Console.Write("Install Package? (y/n):")
+        Dim yndialog = Console.ReadLine()
+        If yndialog = "y" Then
+            Call InstallPKG()
+        ElseIf yndialog = "Y" Then
+            Call InstallPKG()
+        ElseIf yndialog = "n" Then
+            Environment.Exit(0)
+        ElseIf yndialog = "N" Then
+            Environment.Exit(0)
+        Else
+        End If
+    End Sub
     Public Sub InstallPKG()
+        Dim Abspath As String
+        Dim installdir As String
+        Abspath = IO.Path.GetFileNameWithoutExtension(args(2))
+        installdir = "\rootfs\etc\packages\" + Path.GetFileNameWithoutExtension(Abspath)
+        If Directory.Exists("\rootfs") Then
+            Console.WriteLine("Installing " + args(2))
+            Try
+                Directory.CreateDirectory(installdir)
+                ZipFile.ExtractToDirectory(Abspath + ".pkg", "\rootfs\etc\package_cache")
+                If My.Computer.FileSystem.FileExists("\rootfs\etc\package_cache\install.bat") Then
+                    Dim runscriptinfo As New ProcessStartInfo
+                    runscriptinfo.FileName = "\rootfs\etc\package_cache\install.bat"
+                    runscriptinfo.UseShellExecute = True
+                    Process.Start(runscriptinfo).WaitForExit()
+                    File.Copy("\rootfs\etc\package_cache\uninstall.bat", installdir + "\uninstall.bat")
+                    File.Copy("\rootfs\etc\package_cache\package.info", installdir + "\package.info")
+                    Directory.Delete("\rootfs\etc\package_cache", True)
+                    Directory.CreateDirectory("\rootfs\etc\package_cache")
+                Else
+                    Call InvalidPKG()
+                End If
+            Catch ex As Exception
+                Console.WriteLine("Error on installing Package: " + ex.Message)
+            End Try
+
+        Else
+            Console.WriteLine("The command needs to be executed on the Lukindu root disk.")
+        End If
 
     End Sub
-
     Public Sub PKGInfo()
-
+        If File.Exists("\rootfs\etc\packages\" + args(2) + "\package.info") Then
+            Dim filereader As String
+            filereader = My.Computer.FileSystem.ReadAllText("\rootfs\etc\packages\" + args(2) + "\package.info")
+            Console.WriteLine(filereader)
+        Else
+            Console.WriteLine("Package " + args(2) + " is not installed!")
+        End If
     End Sub
 
     Public Sub UninstallPKG()
